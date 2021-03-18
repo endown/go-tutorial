8943aa1b22b5ba82a98b91a01665a7c0949146b1
遠藤隼人 <endouhayato@endouhayatonoMacBook-Pro.local>
2021-03-18 23:17:47 +0900

    struct

--- hello.go
+++ hello.go
@@ -2,76 +2,44 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	// "strconv"
+	// "strings"
 )
 
-//Data is interface for Mydata
-type Data interface {
-	SetValue(vals map[string]string)
-	PrintData()
+type character struct {
+	name        string
+	yearOfBirth int
+	birthPlace  string
 }
 
-//Mydata is Structure
-type Mydata struct {
-	Name string
-	Data []int
+type performance struct {
+	character
+	drama string
 }
 
-//SetValue is Mydata method
-func (md *Mydata) SetValue(vals map[string]string) {
-	md.Name = vals["name"]
-	valt := strings.Split(vals["data"], "")
-	vali := []int{}
-	for _, i := range valt {
-		n, _ := strconv.Atoi(i)
-		vali = append(vali, n)
-	}
-	md.Data = vali
-}
-
-//PrintData is init Mydata method.
-func (md *Mydata) PrintData() {
-	fmt.Println("Name: ", md.Name)
-	fmt.Println("Data: ", md.Data)
-}
-
-//Yourdata is structure.
-type Yourdata struct {
-	Name string
-	Mail string
-	Age  int
-}
-
-//SetValue iis Yourdata method
-func (md *Yourdata) SetValue(vals map[string]string) {
-	md.Name = vals["name"]
-	md.Mail = vals["mail"]
-	n, _ := strconv.Atoi(vals["age"])
-	md.Age = n
-}
-
-//PrintData is init Yourdata method.
-func (md *Yourdata) PrintData() {
-	fmt.Printf("I'm %s. (%d).¥n", md.Name, md.Age)
-	fmt.Printf("mail: %s.¥n", md.Mail)
+func (p performance) profile() {
+	fmt.Println(p.name, "\n出生年:", p.yearOfBirth, "\n出身地:", p.birthPlace, "\n出演ドラマ:", p.drama)
 }
 
 func main() {
-	ob := [2]Data{}
-	ob[0] = new(Mydata)
-	ob[0].SetValue(map[string]string{
-		"name": "Sachiko",
-		"data": "55, 66, 77",
-	})
-	ob[1] = new(Yourdata)
-	ob[1].SetValue(map[string]string{
-		"name": "Mami",
-		"mail": "mami@mume.mo",
-		"age":  "34",
-	})
-	for _, d := range ob {
-		d.PrintData()
-		fmt.Println()
+	p1 := performance{
+		character: character{
+			"新垣結衣",
+			1988,
+			"沖縄県",
+		},
+		drama: "逃げるは恥だが役に立つ",
 	}
+	p2 := performance{
+		character: character{
+			"本田翼",
+			1992,
+			"東京都",
+		},
+		drama: "ラジエーションハウス",
+	}
+	fmt.Println(p1)
+	fmt.Println(p2)
+	p1.profile()
+	p2.profile()
 }
